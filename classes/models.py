0637c56d0b7ae7b92adc22f270779fcc237e25f7
annie-mac <mcavoy.ana@gmail.com>
2025-09-30 19:08:57 +0100

    add classes app, models and update admin

---
+++ classes/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+    
+    def get_friendly_name(self):
+        return self.friendly_name
+    
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    deposit = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    # rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
+    duration = models.IntegerField(null=True, blank=True)
+    location = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f"self.name" ({self.start_time.strftime('%d/%m/%Y')}) 
