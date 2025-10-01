5743f44b15dfce1b9d277a7f441578f37ca3f420
annie-mac <mcavoy.ana@gmail.com>
2025-10-01 20:57:21 +0100

    add classes views, urls, models & admin

--- classes/admin.py
+++ classes/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Class
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('name', 'start_date', 'start_time', 'duration', 'location',
+                    'price', 'deposit', 'image')
+    list_filter = ('start_date', 'location')
+
+admin.site.register(Class, ClassAdmin)
