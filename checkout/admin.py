feda04e8c62b2e611e5ff6ff3e5328a0355097a9
annie-mac <mcavoy.ana@gmail.com>
2025-10-19 13:25:14 +0100

    add forms, sygnals and admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+class OrderLineItemAdmin(admin.TabularInline):
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderLineItemAdmin,)
+
+    readonly_fields = ('order_number', 'date', 'order_total')
+
+    fields = (
+        'order_number', 'full_name', 'email', 'phone_number',
+        'country', 'postcode', 'town_or_city', 'street_address1',
+        'street_address2', 'date', 'order_total'
+    )
+
+    list_display = (
+        'order_number', 'full_name', 'date', 'order_total',
+    )
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
