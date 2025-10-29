06369d55ba2e6016c86877a076f339f7750ca243
annie-mac <mcavoy.ana@gmail.com>
2025-10-29 21:33:08 +0000

    correct file name typo

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse
 from django.contrib import messages
 from django.conf import settings
+from django.urls import reverse
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -28,12 +28,9 @@ def checkout(request):
             'street_address1': request.POST['street_address1'],
             'street_address2': request.POST['street_address2'],
         }
-
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save()
-
-            # Loop through items in the session bag
             for item_id, item_data in bag.items():
                 try:
                     product = Class.objects.get(id=item_id)
@@ -44,34 +41,41 @@ def checkout(request):
                             quantity=item_data,
                         )
                         order_line_item.save()
+                    else:
+                        for size, quantity in item_data['items_by_size'].items():
+                            order_line_item = OrderLineItem(
+                                order=order,
+                                product=product,
+                                quantity=quantity,
+                                product_size=size,
+                            )
+                            order_line_item.save()
                 except Class.DoesNotExist:
                     messages.error(request, (
-                        "One of the classes in your bag wasn't found in our database. "
-                        "Please contact us for assistance!")
+                        "One of the products in your bag wasn't \
+                            found in our database."
+                        "Please call us for assistance!")
                     )
                     order.delete()
                     return redirect(reverse('view_bag'))
 
             request.session['save_info'] = 'save-info' in request.POST
-            return redirect(reverse('checkout_success', args=[order.order_number]))
-
+            return redirect(
+                reverse('checkout_success', args=[order.order_number]))
         else:
-            messages.error(
-                request,
-                'There was an error with your form. Please double check your information.'
-            )
-
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
     else:
         bag = request.session.get('bag', {})
         if not bag:
-            messages.error(request, "There's nothing in your bag at the moment")
-            return redirect(reverse('classes'))
+            messages.error(request, "There's nothing \
+                           in your bag at the moment")
+            return redirect(reverse('products'))
 
         current_bag = bag_contents(request)
-        total = current_bag['total']  # no delivery used here
+        total = current_bag['total']
         stripe_total = round(total * 100)
         stripe.api_key = stripe_secret_key
-
         intent = stripe.PaymentIntent.create(
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
@@ -80,10 +84,8 @@ def checkout(request):
         order_form = OrderForm()
 
         if not stripe_public_key:
-            messages.warning(
-                request,
-                'Stripe public key is missing. Did you forget to set it in your environment?'
-            )
+            messages.warning(request, 'Stripe public key is missing. \
+                Did you forget to set it in your environment?')
 
         template = 'checkout/checkout.html'
         context = {
@@ -101,11 +103,9 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(
-        request,
-        f'Order successfully processed! Your order number is {order_number}. '
-        f'A confirmation email will be sent to {order.email}.'
-    )
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
     if 'bag' in request.session:
         del request.session['bag']
