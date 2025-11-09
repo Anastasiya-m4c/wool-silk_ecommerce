c2bf0c6adf7b3e71d022b2a8e11096ec9384d554
annie-mac <mcavoy.ana@gmail.com>
2025-11-09 21:52:38 +0000

    add email confirmation

--- wool_and_silk/settings.py
+++ wool_and_silk/settings.py
@@ -201,3 +201,5 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+DEFAULT_FROM_EMAIL = 'mcavoy.ana@gmail.com'
