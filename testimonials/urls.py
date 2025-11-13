c23e93fd2f5e1090b4ad161858ac44ed5780ce60
annie-mac <mcavoy.ana@gmail.com>
2025-11-13 19:59:07 +0000

    add testimonials list template & fix url pattern

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.testimonials_list, name='testimonial_list'),
+    path('', views.testimonials_list, name='testimonials'),
     path('submit/', views.submit_testimonial, name='submit_testimonial'),
 ]
