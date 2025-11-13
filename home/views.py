3ff945a96f6e99bc546a484ec77e29d97b19d284
annie-mac <mcavoy.ana@gmail.com>
2025-11-13 21:46:05 +0000

    add a testiomonials carousel to homepage

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+from testimonials.models import Testimonial
 
 def index(request):
     """ index page view """
+    testimonials = Testimonial.objects.filter(is_approved=True)[:5]
+    context = {
+        'testimonials': testimonials,
+    }
+    return render(request, 'home/index.html', context)
 
-    return render(request, 'home/index.html')
