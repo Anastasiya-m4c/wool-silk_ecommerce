0ddcef28e0c88c946cea077b08ec90cc0089a2cc
annie-mac <mcavoy.ana@gmail.com>
2025-11-15 09:00:24 +0000

    add 404 page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from testimonials.models import Testimonial
 
+
 def index(request):
     """ index page view """
     testimonials = Testimonial.objects.filter(is_approved=True)[:5]
@@ -9,3 +10,5 @@ def index(request):
     }
     return render(request, 'home/index.html', context)
 
+def handler404(request, exception):
+    return render(request, '404.html', status=404)
