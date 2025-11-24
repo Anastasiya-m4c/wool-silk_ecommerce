d3f5d1db23e9a96ed2221d73cbfe47a58809efdc
annie-mac <mcavoy.ana@gmail.com>
2025-11-24 20:06:15 +0000

    bug fix - update logict to remove user email from testimonials and allow option to post anonymusly

--- testimonials/forms.py
+++ testimonials/forms.py
@@ -1,13 +1,26 @@
 from django import forms
 from .models import Testimonial
 
+
 class TestimonialForm(forms.ModelForm):
     class Meta:
         model = Testimonial
-        fields = ['title', 'content', 'rating']
+        fields = ['name', 'title', 'content', 'rating']
         widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'Leave blank to post as "Anonymous"'
+            }),
             'content': forms.Textarea(attrs={'rows': 4}),
             'rating': forms.Select(choices=[(i, f'{i} Star{"s" if i > 1 else ""}') for i in range(1, 6)]),
+        }
+        labels = {
+            'name': 'Your Name (optional)',
+            'title': 'Review Title',
+            'content': 'Your Review',
+            'rating': 'Rating'
+        }
+        help_texts = {
+            'name': 'Other visitors will see this name. Leave blank to post anonymously.',
         }
     
     def __init__(self, *args, **kwargs):
--- testimonials/models.py
+++ testimonials/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Testimonial(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(
+        max_length=100,
+        blank=True,
+        default='',
+        help_text="This name will be displayed publicly with your testimonial"
+    )
     title = models.CharField(max_length=200)
     content = models.TextField()
     rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])
@@ -13,4 +20,9 @@ class Testimonial(models.Model):
         ordering = ['-created_at']
     
     def __str__(self):
-        return f"{self.user.username} - {self.title}"
+        return f"{self.get_display_name()} - {self.title}"
+    
+    def get_display_name(self):
+        """Return name if provided, otherwise 'Anonymous'"""
+        return self.name if self.name else "Anonymous"
+
